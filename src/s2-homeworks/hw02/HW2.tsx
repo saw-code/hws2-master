0998ef87e26a547c65853977210799e99ea60106
saw-code <saw.coding@gmail.com>
2023-01-13 23:56:27 +0300

    2_lesson finish

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -48,7 +48,7 @@ function HW2() {
   const [filter, setFilter] = useState<FilterType>('all')
 
   const filteredAffairs = filterAffairs(affairs, filter)
-  const deleteAffairCallback = (_id: number) => { // need to fix any
+  const deleteAffairCallback = (_id: number) => {
     setAffairs(() => deleteAffair(affairs, _id))
   }
 
