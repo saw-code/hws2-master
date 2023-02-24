2c23cd55f047a792faa86842adccf240e102da0a
Dmitry Pilavskiy <saw.coding@gmail.com>
2023-02-24 08:02:00 +0300

    HW-8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,24 @@
 import {UserType} from '../HW8'
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: 'sort'; payload: 'up' | 'down' }
+  | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
+  switch (action.type) {
+    case 'sort': { // by name
+      let sortUp = [...state].sort((a, b) => a.name.localeCompare(b.name))
+      let sortDown = [...state].sort((a, b) => b.name.localeCompare(a.name))
 
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+      return action.payload === "up"
+        ? sortUp : action.payload === 'down'
+          ? sortDown : state
     }
+    case 'check': {
+      let copyState = [...state]
+      return copyState.filter(el => el.age > action.payload) // need to fix
+    }
+    default:
+      return state
+  }
 }
